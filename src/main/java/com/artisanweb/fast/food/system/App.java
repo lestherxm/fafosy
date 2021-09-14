61ef81d14fc525e47a0aa2563e337779c3f1096d
Lesther Xitumul Manuel <lestherxm@gmail.com>
2021-09-14 01:24:27 -0600

    adding a new folder and file xml main was modified

--- src/main/java/com/artisanweb/fast/food/system/App.java
+++ src/main/java/com/artisanweb/fast/food/system/App.java
@@ -1,6 +1,9 @@
 package com.artisanweb.fast.food.system;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -13,14 +16,12 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException {
+        String pathFile = "/fxml/login.fxml";
+            Parent root = FXMLLoader.load(getClass().getResource(pathFile));
+                Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                        stage.show();
     }
 
     public static void main(String[] args) {
